ddf915cbe65a40c323f937cf4a6a8477699bf508
Khang Le <le000184@algonquinlive.com>
2020-11-14 03:18:44 -0500

    First commit

---
+++ Data/AnswerImageDataContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Azure.Storage.Blobs.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Lab6.models;
+
+namespace Data
+{
+    public class AnswerImageDataContext : DbContext
+    {
+        public AnswerImageDataContext(DbContextOptions<AnswerImageDataContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<AnswerImage> AnswerImages { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<AnswerImage>().ToTable("AnswerImage");
+        }
+    }
+}
---
+++ Models/AnswerImage.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab6.models
+{
+    public class AnswerImage
+    {
+        [Required]
+        public  int AnswerImageId
+        {
+            get;
+            set;
+        }
+
+        [Required]
+        public string FileName
+        {
+            get;
+            set;
+        }
+
+        public string Url
+        {
+            get;
+            set;
+        }
+
+    }
+}
